5532d7f0dedbf9980f452c3403434e9b8f296ce2
BernardoPiresMascarenhas <bernardomasca3008@gmail.com>
2024-09-13 22:38:03 -0300

    upload insercao

---
+++ Insercao.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Incercao{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = sc.nextInt();
+        }
+
+        for(int i = 1; i < n; i++){
+            int aux = array[i];
+            int j = i - 1;
+            while(j >= 0 && aux < array[j]){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = aux;
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.print(array[i]);
+        }
+
+
+        sc.close();
+    }
+}
